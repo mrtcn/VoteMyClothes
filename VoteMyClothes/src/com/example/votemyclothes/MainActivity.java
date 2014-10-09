ba829f6cf94d922045c80cb5627832c5390420c7
mrtcn <muratcantuna@gmail.com>
2014-10-10 03:32:03 +1200

    Manifest and main activity import order changes

--- VoteMyClothes/src/com/example/votemyclothes/MainActivity.java
+++ VoteMyClothes/src/com/example/votemyclothes/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.votemyclothes;
 
 import android.app.Activity;
-import android.os.Bundle;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		
 		setContentView(R.layout.activity_main); 
 	}
 }
